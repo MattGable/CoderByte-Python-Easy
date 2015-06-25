e53d38d02a01ee98509633e999b27e487dc22c24
MattGable <MattGable@users.noreply.github.com>
2015-06-25 00:30:47 -0400

    Update 8-CheckNums.py

--- 8-CheckNums.py
+++ 8-CheckNums.py
@@ -1,8 +1,8 @@
 '''
-From CoderByte: Using the Python language, have the function
+From CoderByte: "Using the Python language, have the function
 CheckNums(num1,num2) take both parameters being passed and return the
 string true if num2 is greater than num1, otherwise return the string false.
-If the parameter values are equal to each other then return the string -1. 
+If the parameter values are equal to each other then return the string -1." 
 '''
 
 #1-Compare num1 and num2. Return true as a string if num2 > num1.
